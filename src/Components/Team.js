ac8b4e1c884ead7adb729553dbeb7ead10487f12
aayushali <aayushkhan51@gmail.com>
2021-08-20 17:25:31 +0545

    Teams section

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,3 +1,23 @@
+const teams = [{
+    name: "Mark Lawrence",
+    position: "Web Designer",
+    intro: "Creative, detail-oriented always focused.",
+    image: "url(./images/profile-pic-1.jpeg)"
+},
+{
+    name: "Jane Stenton",
+    position: "SEO Specialist",
+    intro: "Curios, tech geeck and gets serious when it comes to work.",
+    image: "url(./images/profile-pic-2.jpg)"
+
+},
+{
+    name: "Julia Smith",
+    position: "Developer",
+    intro: "Enthusiastic, passionate with great sense of humor.",
+    image: "url(./images/profile-pic-3.jpg)"
+}]
+
 const Team = () => {
 
     return (
@@ -32,12 +52,11 @@ const Team = () => {
                 className="flex justify-center items-center mx-auto py-32"
                 style={{
                     backgroundImage: "url(https://demo.colorlib.com/illdy/wp-content/themes/illdy/layout/images/front-page/pattern.png)",
-                    height: "600px"
                 }}
 
             >
                 <div className="mx-auto">
-                    <div className="pt-20 pb-20 text-center">
+                    <div className="pt-16 pb-16 text-center">
                         <h2 className="text-5xl font-bold text-gray-600 pb-3">Team</h2>
                         <p className="block text-gray-500">
                             Meet the people that are going to take you business to the next
@@ -45,141 +64,54 @@ const Team = () => {
                         </p>
                     </div>
                     <div className="grid grid-cols-3">
-                        <div className="team flex p-10 space-x-8">
-                            <div
-                                className="profile-pic h-32 w-32 rounded-full bg-cover bg-no-repeat"
-                                style={{ backgroundImage: "url(./images/profile-pic-1.jpeg)" }}
-                            ></div>
-                            <div className="description">
-                                <h3 className="text-2xl font-bold text-gray-600 pt-3 pb-2">
-                                    Mark Lawrance
-                                </h3>
-                                <h4 className="text-red-400 pb-4">Web Designer</h4>
-                                <p className="block text-gray-500">
-                                    Creative, detail-oriented always focused.
-                                </p>
-                                <div className="icons flex space-x-3 mt-5 items-center">
-                                    <div className="facebook-icon">
-                                        <object
-                                            data="./images/icons/fb.svg"
-                                            width="30"
-                                            height="30"
-                                            aria-labelledby="fb"
+                        {
+                            teams.map((team) => (
+                                <div className="team flex p-10 space-x-8">
+                                    <div
+                                        className="profile-pic h-32 w-32 rounded-full bg-cover bg-no-repeat"
+                                        style={{ backgroundImage: `${team.image}` }}
+                                    ></div>
+                                    <div className="description">
+                                        <h3 className="text-2xl font-bold text-gray-600 pt-3 pb-2">
+                                            {team.name}
+                                        </h3>
+                                        <h4 className="text-red-400 pb-4">{team.position}</h4>
+                                        <p className="block text-gray-500">
+                                            {team.intro}
+                                        </p>
+                                        <div className="icons flex space-x-3 mt-5 items-center">
+                                            <div className="facebook-icon">
+                                                <object
+                                                    data="./images/icons/fb.svg"
+                                                    width="30"
+                                                    height="30"
+                                                    aria-labelledby="fb"
 
-                                        ></object>
-                                    </div>
-                                    <div className="insta-icon">
-                                        <object
-                                            data="./images/icons/insta.svg"
-                                            width="30"
-                                            height="30"
-                                            className="text-red-400"
-                                            aria-labelledby="fb"
+                                                ></object>
+                                            </div>
+                                            <div className="insta-icon">
+                                                <object
+                                                    data="./images/icons/insta.svg"
+                                                    width="30"
+                                                    height="30"
+                                                    className="text-red-400"
+                                                    aria-labelledby="fb"
 
-                                        ></object>
-                                    </div>
-                                    <div className="linkedin-icon">
-                                        <object
-                                            data="./images/icons/linkedin.svg"
-                                            width="30"
-                                            height="30"
-                                            aria-labelledby="fb"
+                                                ></object>
+                                            </div>
+                                            <div className="linkedin-icon">
+                                                <object
+                                                    data="./images/icons/linkedin.svg"
+                                                    width="30"
+                                                    height="30"
+                                                    aria-labelledby="fb"
 
-                                        ></object>
+                                                ></object>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="team flex p-10 space-x-8">
-                            <div
-                                className="profile-pic h-32 w-32 rounded-full bg-cover bg-no-repeat"
-                                style={{ backgroundImage: "url(./images/profile-pic-2.jpg)" }}
-                            ></div>
-                            <div className="description">
-                                <h3 className="text-2xl font-bold text-gray-600 pt-3 pb-2">
-                                    Jane Stenton
-                                </h3>
-                                <h4 className="text-yellow-400 pb-4">SEO Specialist</h4>
-                                <p className="block text-gray-500">
-                                    Curious, tech-geeck and gets serious when it <br />comes to
-                                    work.
-                                </p>
-                                <div className="icons flex space-x-3 mt-5 items-center">
-                                    <div className="facebook-icon">
-                                        <object
-                                            data="./images/icons/fb.svg"
-                                            width="30"
-                                            height="30"
-                                            aria-labelledby="fb"
-
-                                        ></object>
-                                    </div>
-                                    <div className="insta-icon">
-                                        <object
-                                            data="./images/icons/insta.svg"
-                                            width="30"
-                                            height="30"
-                                            className="text-red-400"
-                                            aria-labelledby="fb"
-
-                                        ></object>
-                                    </div>
-                                    <div className="linkedin-icon">
-                                        <object
-                                            data="./images/icons/linkedin.svg"
-                                            width="30"
-                                            height="30"
-                                            aria-labelledby="fb"
-
-                                        ></object>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div className="team flex p-10 space-x-8">
-                            <div
-                                className="profile-pic h-32 w-32 rounded-full bg-cover bg-no-repeat"
-                                style={{ backgroundImage: "url(./images/profile-pic-3.jpg)" }}
-                            ></div>
-                            <div className="description">
-                                <h3 className="text-2xl font-bold text-gray-600 pt-3 pb-2">
-                                    John Smith
-                                </h3>
-                                <h4 className="text-red-400 pb-4">Developer</h4>
-                                <p className="block text-gray-500">
-                                    Enthusiastic, passionate with great sense of humor.
-                                </p>
-                                <div className="icons flex space-x-3 mt-5 items-center">
-                                    <div className="facebook-icon">
-                                        <object
-                                            data="./images/icons/fb.svg"
-                                            width="30"
-                                            height="30"
-                                            aria-labelledby="fb"
-                                        ></object>
-                                    </div>
-                                    <div className="insta-icon">
-                                        <object
-                                            data="./images/icons/insta.svg"
-                                            width="30"
-                                            height="30"
-                                            className="text-red-400"
-                                            aria-labelledby="ins"
-                                        ></object>
-                                    </div>
-                                    <div className="linkedin-icon">
-                                        <object
-                                            data="./images/icons/linkedin.svg"
-                                            width="30"
-                                            height="30"
-                                            aria-labelledby="linkedIn"
-                                        ></object>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                            ))}
                     </div>
                 </div>
             </section>
